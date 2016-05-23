58c721a95b8b34d4403f26d26c794b896ae9b903
Oliver <obsta2010@hotmail.com>
2016-05-23 20:42:01 +1200

    Moved base query to authentication, added start/stop/set speed/remove

--- authentication.php
+++ authentication.php
@@ -5,5 +5,16 @@ $user = "lombardi";
 $pass = "Rev0lt1x01";
 $connection   = "192.168.1.102:1338";
   
+//base command, used by all API.
+function generateQuery($command)
+{
+    global $user,
+           $pass,
+           $connection;
+    
+    exec('transmission-remote '.$connection.' --auth  '.$user.':'.$pass.' '.$command, $output);
+    return $output;
+}
+
     
 ?>
--- transd-lib.php
+++ transd-lib.php
@@ -3,19 +3,13 @@
 error_reporting(1);
 require_once("authentication.php");
 
-
-//====== API ========
-
-//Create command
-function generateQuery($command)
-{
-    global $user,
-           $pass,
-           $connection;
-    
-    exec('transmission-remote '.$connection.' --auth  '.$user.':'.$pass.' '.$command, $output);
-    return $output;
-}
+//============= API ============//
+// Where the paramater $id      //
+// is expected; values such     //
+// as: '2,3,4' or '2-4'         //
+// can be passed, representing  //
+// torrents from 2 to 4         //
+//==============================//
 
 //Returns a specific torrent via its array index.
 function getTorrent($id)
@@ -29,8 +23,8 @@ function getAllTorrents()
     return generateQuery('-l');
 }
 
-//Adds torrent via URL+Autostart, returns FAIL/SUCESS
-function addTorrent($url, $autostart = false)
+//Adds torrent via URL+Autostart, returns FAIL/SUCCESS
+function addTorrent($url, $autostart = true)
 {
     $autostart = "";
     
@@ -42,5 +36,46 @@ function addTorrent($url, $autostart = false)
     return generateQuery($start." --add ".$url);
 }
 
-//=====================
+//Remove a torrent via id, if delete is true then also remove related data, returns FAIL/SUCCESS
+function removeTorrent($id, $delete = false)
+{
+    if($id != NULL)
+        return generateQuery('-t'.$id.' -r');
+    else if($id != NULL && $delete == true)
+        return generateQuery('-t'.$id.' -R');
+    
+}
+
+//Sets the Upload/Download speed, if 0 then no limit is applied.
+function setSpeed($down = NULL, $up = NULL)
+{
+    $append = "";
+    
+    if($down != NULL)
+       $append.=' -d'.$down;
+    else if($down == 0)
+        $append.=' -D';
+    
+    if($up != NULL)
+       $append.=' -u'.$up;
+    else if($up == 0)
+        $append.=' -U';
+    
+    return generateQuery($append);
+    
+}
+
+//Start the selected torrent
+function start($id)
+{
+    return generateQuery('-t'.$id.' -s');
+}
+
+//Stop the selected torrent
+function stop($id)
+{
+    return generateQuery('-t'.$id.' -S');
+}
+
+
 ?>
